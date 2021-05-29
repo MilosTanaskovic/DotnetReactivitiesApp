9a2ac33918e0117906161a469952610cf8dce4e2
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2021-05-29 07:57:24 +0200

    creating a detials view...

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Button, Card, Icon, Image } from 'semantic-ui-react'
+import { Activity } from '../../../app/modules/activity'
+
+interface Props {
+    activity: Activity
+}
+
+export default function ActivityDetails({activity}: Props) {
+    const { category, title, date, description } = activity;
+    return (
+        <Card fluid>
+            <Image src={`/assets/categoryImages/${category}.jpg`} />
+            <Card.Content>
+            <Card.Header>{title}</Card.Header>
+            <Card.Meta>
+                <span>{date}</span>
+            </Card.Meta>
+            <Card.Description>
+                {description}
+            </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <Button.Group widths="2">
+                    <Button basic color="blue" content="Edit" />
+                    <Button basic color="grey" content="Cancel" />
+                </Button.Group>
+            </Card.Content>
+        </Card>
+    )
+}
