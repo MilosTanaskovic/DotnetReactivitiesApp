f92a8cd49c04b744fc76a6446ea9cdda71d69b0d
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2021-07-17 17:23:45 +0200

    Persisiting the login, sending up the token with the request, adding Modals...

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -20,7 +20,8 @@ export default class UserStore {
           const user = await agent.Account.login(creds);
           store.commonStore.setToken(user.token);
           runInAction(() => this.user = user );
-          history.push('/activities');  
+          history.push('/activities'); 
+          store.modalStore.closeModal();
         } catch (error) {
             throw error;
         }
@@ -31,5 +32,14 @@ export default class UserStore {
         window.localStorage.removeItem('jwt');
         this.user = null;
         history.push('/');
+    }
+
+    getUser = async () => {
+        try {
+            const user = await agent.Account.current();
+            runInAction(() => this.user = user);
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
