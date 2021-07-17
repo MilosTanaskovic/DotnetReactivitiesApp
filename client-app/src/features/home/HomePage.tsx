ec62d465db9c6ef9e693dbc94223df61997e82bf
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2021-07-17 16:31:25 +0200

    updating the home page and nav bar...

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,21 +1,35 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Container, Header, Image, Segment } from 'semantic-ui-react'
+import { useStore } from '../../app/stores/store';
 import './HomePage.css';
 
-export default function HomePage() {
+export default observer(function HomePage() {
+    const {userStore} = useStore();
     return (
        <Segment inverted textAlign="center" vertical className="masthead">
            <Container text>
                 <Header as='h1' inverted>
                     <Image size="massive" src="/assets/logo.png" alt="CodeDancing Activities" style={{marginBottom: 12}} />
-                    CodeDancing Activities
+                    CodeDancing <br />
+                    ⛹️‍♀️ 🤺 🤾‍♂️ 🏌️‍♀️ 🏇 🧘‍♀️ 🏄‍♂️ 🏊‍♀️ 🤽‍♀️ 🚣‍♂️ 🧗‍♀️ 🚴‍♂️ <br/>
+                    🏆
                 </Header>
-                <Header as="h2" inverted content="Welcome to CodeDancing Activities" />
-                <Button as={Link} to="/login" size='huge' inverted>
-                    Login!
-                </Button>
+                {userStore.isLoggedIn ? (
+                    <>
+                        <Header as="h2" inverted content="Welcome to CodeDancing Activities" />
+                        <Button as={Link} to="/activities" size='huge' inverted>
+                            Go To Activities
+                        </Button>
+                    </>
+                    
+                ) : (
+                        <Button as={Link} to="/login" size='huge' inverted>
+                            Login!
+                        </Button>
+                )}
            </Container>
        </Segment>
     )
-}
+});
